69fae4a6f8b768a67976b4934a91969fc6e4a67c
Dieter1706 <DieterDDHT@gmail.com>
2021-07-27 19:05:29 -0400

    Dev 0.3
    Se agregaron los html de cada pagina para poder editarlas por separado y con la pagina principalavanzada

--- farmacia.py
+++ farmacia.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")#Indica que será la pagina de inicio
 def inicio():
 	#return render_template("tema.html", contenido = nombre, otro ="otro nombre")
-	return render_template("pagina.html", contenido = "Test")
+	return render_template("paginaprincipal.html", contenido = "Test")
 
 if __name__ == "__main__":
 	app.run(debug=True)
