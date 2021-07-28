71ecf1b9a6820d9c7db661058bd0e798061d0b57
Dieter1706 <DieterDDHT@gmail.com>
2021-07-28 15:30:14 -0400

    Dev 0.4
    Incorporación del header y la posibilidad de viajar entre paginas

--- farmacia.py
+++ farmacia.py
@@ -2,10 +2,18 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")#Indica que será la pagina de inicio
+@app.route("/")
 def inicio():
-	#return render_template("tema.html", contenido = nombre, otro ="otro nombre")
 	return render_template("paginaprincipal.html", contenido = "Test")
+
+@app.route("/comprar.html")
+def comprar():
+	return render_template("comprar.html", contenido ="test")
+
+@app.route("/login.html")
+def login():
+	return render_template("login.html", contenido ="test")
 
 if __name__ == "__main__":
 	app.run(debug=True)
+
